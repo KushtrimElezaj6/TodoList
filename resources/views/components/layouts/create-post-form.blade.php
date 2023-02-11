568d8be63b5f7075cf8d39fc6134365eb6cac039
KushtrimElezaj6 <107043851+KushtrimElezaj6@users.noreply.github.com>
2023-02-12 00:08:04 +0100

    Update create form

--- resources/views/components/layouts/create-post-form.blade.php
+++ resources/views/components/layouts/create-post-form.blade.php
@@ -1,18 +1,19 @@
 @props(['priorities'])
+@props(['tags'])
 
 <form action="/todo" method="POST">
     @csrf
     <div class=" float-left gap-6 mb-6 md:grid-cols-2">
         <div>
-            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">TITLE</label>
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">TITLE</label>
             <input type="string" name="title" value="{{ old('title') }}" id="first_name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5"
                 placeholder="Title" required>
         </div>
         <div>
-            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">CONTENT</label>
+            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900">CONTENT</label>
             <input type="text" name="content" value="{{ old('content') }}" id="last_name"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5"
                 placeholder="Content" required>
         </div>
        
@@ -30,11 +31,27 @@
                
             </select>
         </div>
-        <h1 class="text-l font-italic dark:text-white   "> Choose a time </h1>
+
+        <div class="flex-1">
+            <label for="tag" class="mb-0.3 block text-sm font-medium text-gray-600">Select Tags
+            </label>
+            <select id="select" name="tags[]" multiple
+                class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 mb-2 text-grey-darker">
+                <option class="hidden" disabled selected> Select your Tags
+                </option>
+
+                @foreach ($tags as $tag)
+                <option value="{{ $tag->id }}">{{ $tag->tag }}</option>
+
+            @endforeach
+               
+            </select>
+        </div>
+        <h1 class="text-l font-italic   "> Choose a time </h1>
 
         <input type="datetime-local" id="meeting-time" name="due_date" value="">
         <button type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">CREATE
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center">CREATE
             TASK </button>
-
+    </div>
 </form>
