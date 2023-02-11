05871ea26e7f04aa642c1390a7217317cfb8738f
KushtrimElezaj6 <107043851+KushtrimElezaj6@users.noreply.github.com>
2023-02-12 00:12:31 +0100

    Add edit tags function

--- resources/views/todo/edit.blade.php
+++ resources/views/todo/edit.blade.php
@@ -19,21 +19,20 @@
         @method('PATCH')
         <div class="grid gap-6 mb-6 md:grid-cols-2">
             <div>
-                
-                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">TITLE</label>
+
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">TITLE</label>
                 <input type="string" name="title" value="{{ old('title', $todo['title']) }}" id="first_name"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
                     placeholder="Title" required>
             </div>
             <br>
             <div>
-                <label for="last_name"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">CONTENT</label>
+                <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900">CONTENT</label>
                 <input type="text" name="content" value="{{ old('content', $todo['content']) }}" id="last_name"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
                     placeholder="Content" required>
             </div>
-           
+
     </form>
     <br>
     <div>
@@ -47,7 +46,8 @@
             option
             <select id="priority" name="priority"
                 class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 mb-2 text-grey-700">
-                <option class="hidden" value="{{ old('priority', $todo['priority'])}}" disabled selected>Select your priority
+                <option class="hidden" value="{{ old('priority', $todo['priority']) }}" disabled selected>Select your
+                    priority
                 </option>
                 @foreach ($priorities as $priority)
                     <option>{{ $priority }}</option>
@@ -56,27 +56,42 @@
         </label>
     </div>
 
-    <button type="submit"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    <div class="flex-1">
+        <label for="tag" class="mb-0.3 block text-sm font-medium text-gray-600">Select Tags
+        </label>
+        <select id="tag" name="tags[]" multiple
+            class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 mb-2 text-grey-darker">
+            <option class="hidden" value="{{ old('tag', $todo['tag']) }}"selected> Select your Tags
+            </option>
+            @foreach ($todo->tags as $tag)
+                <option value="">
+
+                    {{ $tag->tag }}
+                </option>
+            @endforeach
+
+        </select>
+        <form action="/todo/{{ $todo['id'] }}/completed " method="POST">
+            @csrf
+            @method('PATCH')
+
+            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded-full">
+
+                completed
+
+            </button>
+            <br><br>
+            <div class="bg-blue-500 hover:bg-red-700 text-white  py-2 px-4 rounded-full">
+                Completed: {{ $todo->completed_at ?? 'Not completed' }}
+            </div>
+            <br>
+
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4  focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center">
                 UPDATE TASK
-                </button>
-                <br><br>
-
-
-    <form action="/todo/{{ $todo['id'] }}/completed " method="POST">
-        @csrf
-        @method('PATCH')
-
-        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded-full">
-
-            completed
-
-        </button>
-        <br><br>
-        <div class="bg-blue-500 hover:bg-red-700 text-white  py-2 px-4 rounded-full">
-            Completed: {{ $todo->completed_at ?? 'Not completed' }}
-        </div>
-    </form>
+            </button>
+            <br><br>
+        </form>
 
 
 
