38b911b7213032f1ed530b17e7d950bf2eb0f4c0
KushtrimElezaj6 <107043851+KushtrimElezaj6@users.noreply.github.com>
2023-02-12 00:06:51 +0100

    update table

--- database/migrations/2023_01_04_121239_create_todos_table.php
+++ database/migrations/2023_01_04_121239_create_todos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            
             $table->string('title', 255);
             $table->text('content');
             $table->date('completed_at')->nullable();
