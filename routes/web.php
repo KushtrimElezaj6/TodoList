5a22310077442b27115f1009e11ff6df4b3b991c
KushtrimElezaj6 <107043851+KushtrimElezaj6@users.noreply.github.com>
2023-02-12 00:13:07 +0100

    Add auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +17,37 @@ use App\Http\Controllers\TodoController;
 |
 */
 
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
 Route::get('/', [Todocontroller::class, 'index'])->name('todo.index');
 Route::get('/todo/edit/{todo}', [Todocontroller::class, 'edit']);
 Route::post('/todo', [Todocontroller::class, 'store']);
 Route::patch('/todo/{todo}', [Todocontroller::class, 'update']);
 Route::delete('/todo/{todo}', [Todocontroller::class, 'destroy']);
 Route::patch('/todo/{todo}/completed', [TodoController::class, 'completed']);
-Route::get('/tags', [TagController::class , 'index'])->name('tag.index');
+
+
+Route::get('/tags', [Tagcontroller::class, 'index'])->name('tags.index');
+Route::post('/tags', [Tagcontroller::class, 'store']);
+Route::delete('/tags/{tag}', [Tagcontroller::class, 'destroy']);
+Route::get('/tags/edit/{tag}', [Tagcontroller::class, 'edit']);
+Route::patch('/tags/{tag}', [Tagcontroller::class, 'update']);
+
+});
+
+
+
+
+
+
+require __DIR__.'/auth.php';
